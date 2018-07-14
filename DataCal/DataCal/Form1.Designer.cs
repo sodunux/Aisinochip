50403fd3940e09b056764cbe030ec4e50bd22ae9
Sodunux <sodunux@163.com>
2018-07-15 02:23:16 +0800

    增加打开hex和bin文件功能，并增加CRC16功能。

---
+++ DataCal/DataCal/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace DataCal
+{
+    partial class DataCal
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataCal));
+            this.maintab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.EE_Addr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EE_Data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OpenFileBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.CrcBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maintab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // maintab
+            // 
+            this.maintab.Controls.Add(this.tabPage1);
+            this.maintab.Controls.Add(this.tabPage2);
+            this.maintab.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.maintab.Location = new System.Drawing.Point(0, 0);
+            this.maintab.Name = "maintab";
+            this.maintab.SelectedIndex = 0;
+            this.maintab.Size = new System.Drawing.Size(729, 411);
+            this.maintab.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(721, 385);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DataView";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(860, 385);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Card";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(715, 379);
+            this.splitContainer1.SplitterDistance = 180;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EE_Addr,
+            this.EE_Data});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(531, 379);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // EE_Addr
+            // 
+            this.EE_Addr.HeaderText = "地址";
+            this.EE_Addr.Name = "EE_Addr";
+            // 
+            // EE_Data
+            // 
+            this.EE_Data.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.EE_Data.HeaderText = "数据";
+            this.EE_Data.Name = "EE_Data";
+            // 
+            // OpenFileBtn
+            // 
+            this.OpenFileBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OpenFileBtn.Location = new System.Drawing.Point(85, 3);
+            this.OpenFileBtn.Name = "OpenFileBtn";
+            this.OpenFileBtn.Size = new System.Drawing.Size(92, 40);
+            this.OpenFileBtn.TabIndex = 1;
+            this.OpenFileBtn.Text = "打开文件";
+            this.OpenFileBtn.UseVisualStyleBackColor = true;
+            this.OpenFileBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 98F));
+            this.tableLayoutPanel1.Controls.Add(this.OpenFileBtn, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.CrcBtn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.40984F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.59016F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(180, 171);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 209);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(180, 170);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // CrcBtn
+            // 
+            this.CrcBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CrcBtn.Location = new System.Drawing.Point(3, 3);
+            this.CrcBtn.Name = "CrcBtn";
+            this.CrcBtn.Size = new System.Drawing.Size(76, 40);
+            this.CrcBtn.TabIndex = 2;
+            this.CrcBtn.Text = "计算CRC";
+            this.CrcBtn.UseVisualStyleBackColor = true;
+            this.CrcBtn.Click += new System.EventHandler(this.CrcBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "6363";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(85, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(92, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "00000010";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CRC初始值";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(85, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "EndAddr";
+            // 
+            // DataCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(729, 411);
+            this.Controls.Add(this.maintab);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DataCal";
+            this.Text = "DataCal";
+            this.Load += new System.EventHandler(this.DataCal_Load);
+            this.maintab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl maintab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EE_Addr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EE_Data;
+        private System.Windows.Forms.Button OpenFileBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button CrcBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DataCal/DataCal/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataCal
+{
+    public partial class DataCal : Form
+    {
+        byte[] databuff = new byte[1024 * 1024];
+        int databufflen = 0;
+
+        public DataCal()
+        {
+            InitializeComponent();
+        }
+
+        private void DataCal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string DeleteSpaceString(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            hexString = hexString.Replace("\t", "");
+            if ((hexString.Length % 2) != 0)
+                hexString = "0" + hexString; //如果最后不足两位，最后添“0”。
+
+            return hexString;
+        }
+        public static byte[] strToHexByte(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0)
+                hexString = "0" + hexString;  //如果最后不足两位，最后添“0”。
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+        public static string byteToHexStr(int len, byte[] bytes)
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    returnStr += bytes[i].ToString("X2");
+                }
+            }
+            return returnStr;
+        }
+
+        public void showMessage(string tempstr) 
+        {
+            richTextBox1.Text += tempstr;
+            richTextBox1.Text += "\n";
+        }
+
+        public int calcrc(byte[] data,int initvalue,int len)
+        {
+            int i,j;
+            int result;
+            result = initvalue;
+            for (i = 0; i < len; i++)
+            {
+                result = result ^ data[i];
+                for (j = 0; j < 8; j++)
+                {
+                    if ((result & 0x0001) == 0x0001)
+                        result = (result >> 1) ^ 0x8408;
+                    else
+                        result = result >> 1;
+                }
+            }
+            return result;
+        }
+
+        void updatedatabuff()
+        {
+            int i,j;
+            string tempstr;
+            databufflen = 0;
+            byte[] tempbuff=new byte[16];
+            try
+            {
+                for (i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    
+                    tempstr = dataGridView1[1, i].Value.ToString();
+                    tempstr = DeleteSpaceString(tempstr);
+                    tempbuff = strToHexByte(tempstr);
+                    for (j = 0; j < 16; j++) 
+                    {
+                        databuff[databufflen + j] = tempbuff[j];
+                    }
+                    databufflen += 16;    
+                }
+            }
+            finally 
+            {
+                //showMessage("读取缓存区数据错误");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename;
+            string tempstr;
+            string[] rowcode;
+            int i,j;
+            string addrstr;
+            string datacountstr;
+            string szHex;
+            int hexbaseaddr;
+            byte[] bytedata;
+            int addr;
+
+
+            openFileDialog1.Filter = "hex files (*.hex;*.bin)|*.bin;*.hex|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            filename = openFileDialog1.FileName;
+            tempstr = filename.Substring(filename.Length - 4, 4);
+            tempstr = tempstr.ToUpper();
+            hexbaseaddr = 0;
+            try 
+            {
+                if (tempstr == ".HEX")
+                {
+                    rowcode = File.ReadAllLines(filename);
+
+                    for (i = 0; i < rowcode.Length; i++)
+                    {
+                        szHex = "";
+                        tempstr = rowcode[i];
+                        if (tempstr == "")
+                            continue;
+                        else if (tempstr.Substring(0, 1) == ":")
+                        {
+                            if (tempstr.Substring(1, 8) == "00000001")
+                                break;
+                            else if (tempstr.Substring(7, 2) == "02") //段偏移 地址偏移4bits
+                            {
+                                hexbaseaddr = Convert.ToUInt16(tempstr.Substring(9, 4), 16) << 0x04;
+                                continue;
+                            }
+                            else if (tempstr.Substring(7, 2) == "04") //线性段偏移 地址偏移16bits
+                            {
+                                hexbaseaddr = Convert.ToUInt16(tempstr.Substring(9, 4), 16) << 16;
+                            }
+                            else if (tempstr.Substring(7, 2) == "05")
+                            {
+                            }
+                            else if (tempstr.Substring(7, 2) == "03") { }
+                            else
+                            {
+                                addrstr = tempstr.Substring(3, 4);//记录该行地址
+                                datacountstr = tempstr.Substring(1, 2);//记录该行的字节个数
+                                szHex += tempstr.Substring(9, tempstr.Length - 11);//读取数据
+                                bytedata = strToHexByte(szHex);
+                                //addr = hexbaseaddr + (int)(strToHexByte(addrstr)[1]) + (int)(strToHexByte(addrstr)[0] * 256);
+                                addr = (int)(strToHexByte(addrstr)[1]) + (int)(strToHexByte(addrstr)[0] * 256);
+                                if (addr >= databufflen)
+                                {
+                                    databufflen = addr + strToHexByte(datacountstr)[0];
+                                }
+                                for (j = 0; j < strToHexByte(datacountstr)[0]; j++)
+                                {
+                                    databuff[addr + j] = bytedata[j];
+                                }
+                            }
+                        }
+                    }
+                    databufflen = ((databufflen + 3) / 4) * 4; //文件大小取4的倍数
+                }
+                else if (tempstr == ".BIN")
+                {
+                    FileStream s2 = File.OpenRead(filename);
+                    databufflen = (int)s2.Length;
+                    s2.Read(databuff, 0, databufflen);
+                    s2.Close();
+                    databufflen = ((databufflen + 3) / 4) * 4;
+                }
+                else { }
+
+                //显示数据
+                dataGridView1.Rows.Clear();//清除数据buffer
+                if (databufflen == 0) dataGridView1.RowCount = 1;
+                else dataGridView1.RowCount = ((databufflen + 15) / 16);
+
+                for (i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    dataGridView1[0, i].Value = (i * 16 + hexbaseaddr).ToString("X8");
+                    tempstr = "";
+                    for (j = 0; j < 0x10; j++)
+                    {
+                        tempstr = tempstr + databuff[i * 16 + j].ToString("X2") + " ";
+                    }
+                    dataGridView1[1, i].Value = tempstr;
+                }
+            }
+            finally
+            {
+                //showMessage("打开文件失败");
+            }
+            
+         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CrcBtn_Click(object sender, EventArgs e)
+        {
+            byte []endaddrbytes=new byte[4];
+            byte []initvaluebytes=new byte[4];
+            int len,initvalue;
+            int crcvalue;
+            string tempstr;
+            try 
+            {
+                updatedatabuff();
+                
+                endaddrbytes=strToHexByte(textBox2.Text);
+                initvaluebytes=strToHexByte(textBox1.Text);
+                initvalue=(int)initvaluebytes[1] + (int)initvaluebytes[0] * 256;
+                len=(int)(endaddrbytes[3]) + (int)(endaddrbytes[2] <<8)+(int)(endaddrbytes[1] <<16)+(int)(endaddrbytes[2] <<24)+1;
+                crcvalue=calcrc(databuff, initvalue, len);
+                tempstr = "CRC初始值：" + initvalue.ToString("X8")+"\n";
+                tempstr += "CRC计算长度：" + len.ToString("X8") + "\n";
+                tempstr += "CRC计算结果：" + crcvalue.ToString("X8");
+                showMessage(tempstr);       
+            }
+            finally {
+                //showMessage("CRC计算错误");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+
+
+    }
+
+}
